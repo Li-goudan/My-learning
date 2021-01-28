f88496a2e112032a2c6bdd43f37d0675cfb47915
goudaner <Goudan.er@outlook.com>
2021-01-28 19:24:37 +0800

    Add the UIDesign project

---
+++ app/src/main/java/com/example/uidesign/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.uidesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageButton btn=findViewById(R.id.imageButton);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
